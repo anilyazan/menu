d6f3744636a8609489833557200de5b9743ff964
anilyazan <anilyazan99@gmail.com>
2023-07-11 16:48:31 +0300

    fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <div className="footer">
       <div className="socialMedia"> </div>
-      <p> &copy;  Hot Burgerss </p>
+      <p> &copy;  Hot Burgers </p>
     </div>
   );
 }
